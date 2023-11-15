e7703e9bb0808083bd9b6633c6fd47e687ba31b6
Shashank-Sahu8 <shashanksahu87070@gmail.com>
2023-11-15 12:28:17 +0530

    News App

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: splash_screen(),
+    );
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class splash_screen extends StatefulWidget {
+  const splash_screen({super.key});
+
+  @override
+  State<splash_screen> createState() => _splash_screenState();
+}
+
+class _splash_screenState extends State<splash_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
